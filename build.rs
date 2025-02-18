23b1d0c052e13cfa01c8be8a1505ae71dc56b55d
Haoze Xie <royenheart@gmail.com>
2025-02-18 19:35:50 +0800

    build: Add AUTH_SERVER_IP env default value in config

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::{env, net::Ipv4Addr};
 fn main() {
     let auth_server_ip = env!(
         "AUTH_SERVER_IP",
-        "Expect env AUTH_SERVER_IP, export AUTH_SERVER_IP=10.0.0.1"
+        "Expect env AUTH_SERVER_IP, export AUTH_SERVER_IP=10.0.0.1 or set it in .cargo/config.toml"
     );
     println!("ENV AUTH_SERVER_IP = {auth_server_ip}");
     auth_server_ip
